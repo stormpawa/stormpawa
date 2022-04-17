dab546664fd20cbd550ca94dec54023c69c867ad
stormpawa <103898197+stormpawa@users.noreply.github.com>
2022-04-17 13:49:35 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+BOT_TOKEN = '1722354800:AAGUhcgUbq1hUZKdMhkcioaPm_Lf91xrpto' #token de tu bot
+API_ID =  7739789 # lo creas en https://my.telegram.org/
+API_HASH = '0dac59387c482f674410f79636a09f1f'  # lo creas en https://my.telegram.org/
+OWNER = [1291931851]
+
+# DATOS DEL MOODLE
+USUARIO = 'kakyoin'
+MOODLE_PASSWORD = 'Micasaesgrande*123'
+USUARIO_ID =  6838 #Tu id de la nube
+ZIP_MB = 250
+MOODLE_URL = 'aulacened.uci.cu'
+
+# CUENTA DE MEGA - este bot sirve par mega tambien
+GMAIL_MEGA = '@gmail.com' 
+PASS_MEGA = 'contraseña'
